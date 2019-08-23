5260d09d82adac6fb1229d067bae346b19ef5776
Miguel Jimenez <accounts@migueljimenez.co>
2019-08-23 08:54:14 -0700

    Fix bug

--- src/main/java/ingesoft/App.java
+++ src/main/java/ingesoft/App.java
@@ -5,7 +5,7 @@ package ingesoft;
 
 public class App {
     public String getGreeting() {
-        return null;
+        return "Hello world.";
     }
 
     public static void main(String[] args) {
